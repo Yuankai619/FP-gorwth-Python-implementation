305b5bc89fca992cc73716ebcb1d7a742e8e5a1a
Yuankai619 <Yuankai_@outlook.com>
2023-11-29 09:11:58 +0800

    update Broute_normal.py

--- Broute_normal.py
+++ Broute_normal.py
@@ -9,41 +9,41 @@ with open(file_path, 'r') as file:
         data_list.append(row)
 
 
-# def findMaxbylist(data):
-#     MAX_val=0
-#     for i in data:
-#         for j in i:
-#             MAX_val+=j
-#             # MAX_val=max(MAX_val,j)
-#     print(MAX_val)
-
-# findMaxbylist(data_list)
-# arr= [0 for _ in range(121)]
 p1= [0 for _ in range(121)]
 for i in data_list:
     for j in i:
         p1[j]+=1
+cnt2=0
+for i in range(1,120):
+    if p1[i]>=813:
+        cnt2+=1
+print(cnt2)
+
+
 p2=[[0 for _ in range(121)] for _ in range(121)]
-# support=[]
-# for id,i in enumerate(arr):
-#     if i >=813:
-#         support.append(id)
+
 for i in data_list:
-    for j in range(1,120):
-        st=set(i)
-        for k in st:
+    for j in i:
+        for k in i:
             p2[j][k]+=1
+
+# data[]
+# for(int j=1;j<data[i].size();j++){
+#     for(int k=j+1;k<=data[i].size();k++){
+#         p2[j][k];
+#     }
+# }
         
 cnt = 0
 for i in range(1,120):
     for j in range(1,120):
         if i == j:
             continue
-        elif p1[i]>=813 and ((p2[i][j])/p1[i])>0.8:
+        elif p2[i][j]>=813:
             cnt+=1
 
 
-print(cnt)
+print(cnt/2)
 
 
 end_time=time.time()
