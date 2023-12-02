ddf84a9969c430d2690f0c0cc3d88774649f5ab4
Yuankai619 <Yuankai_@outlook.com>
2023-12-03 04:54:39 +0800

    update final version

--- FP growth Final.py
+++ FP growth Final.py
@@ -2,7 +2,6 @@ from collections import defaultdict
 from itertools import combinations
 from multiprocessing import Pool
 import time
-
 CPU_COUNT = 24
 PATTERN_LEN = 10     #最多一次看5個item   
 MIN_SUPPORT = 813    #最少出現次數
@@ -26,19 +25,14 @@ class Node:
         self.child = {}
         self.nextSimilarItem = None
         
-    #計算出現次數    
-    def inc(self, num):
-        self.count += num
-        
     #印出樹的路徑       
     # def display(self, ind=1):
     #     print ('  '*ind, self.name, ' ', self.count)
     #     for child in self.child.values():
     #         child.display(ind+1)
-              
-        
+       
 def createFPtree(dataset):
-    headPointTable = {}
+    headPointTable = {}#key = 元素的值,value = list(元素的數量,第一個該元素的node(也就是起點node))
     
     #計算出現次數
     for data in dataset:
@@ -47,7 +41,7 @@ def createFPtree(dataset):
     
     #只留下>=MIN_SUPPORT的item
     headPointTable = {name:cnt for name, cnt in headPointTable.items() if cnt >= MIN_SUPPORT}
-    freq_items = set(headPointTable.keys())
+    freq_items = list(headPointTable.keys())
 
     #沒有任何元素
     if len(freq_items)==0: return None, None
@@ -56,7 +50,7 @@ def createFPtree(dataset):
         headPointTable[k] = [headPointTable[k], None]  #指到下一個similiar Item
         
     #建立fp tree
-    fp_tree = Node('null set', 1, None) 
+    cur_root = Node(-1, 1, None) 
     
     #把資料看過第二遍
     for items, cnt in dataset.items():
@@ -64,42 +58,37 @@ def createFPtree(dataset):
         
         #只留下出現在freq_items的item
         for item in items:
-            if item in freq_items:
+            if item in freq_items:# 如果有>=MIN_SUPPORT,item是元素的值
                 keep_freq_items[item] = headPointTable[item][0]
             
         if len(keep_freq_items) > 0:
             #用item出現次數，由大到小排序
-            orderedFrequentItems = [v[0] for v in sorted(keep_freq_items.items(),key = lambda v: (v[1], v[0]),reverse = True)]
+            keep_freq_items = [v[0] for v in sorted(keep_freq_items.items(),key = lambda v: (v[1], v[0]),reverse = True)]
             #更新FP tree
-            updateFPtree(orderedFrequentItems, fp_tree, headPointTable, cnt)
+            updateFPtree(keep_freq_items, cur_root, headPointTable, cnt)
         
-    return fp_tree, headPointTable
+    return cur_root, headPointTable
     
-def updateFPtree(items, fp_tree, headPointTable, cnt):
-    if items[0] in fp_tree.child:
-        fp_tree.child[items[0]].inc(cnt)
+def updateFPtree(data, cur_root, headPointTable, cnt):
+    if data[0] in cur_root.child:
+        cur_root.child[data[0]].count+=cnt
     else:
-        #如不存在子節點，新增一個新的子節點
-        fp_tree.child[items[0]] = Node(items[0], cnt, fp_tree)
-        #指到下一個
-        if headPointTable[items[0]][1] == None:  
-            headPointTable[items[0]][1] = fp_tree.child[items[0]]
-        else:
-            updateHeadPointTable(headPointTable[items[0]][1], fp_tree.child[items[0]])
+        cur_root.child[data[0]] = Node(data[0], cnt, cur_root)#如不存在子節點，新增一個新的子節點
+        if headPointTable[data[0]][1] == None:#當第一次插入這個值的node  
+            headPointTable[data[0]][1] = cur_root.child[data[0]]
+        else:#如果已經插入過這個值，就要把HeadPointTable串起來
+            updateHeadPointTable(headPointTable[data[0]][1], cur_root.child[data[0]])#(原本的點,新插入的點)
     
     #遞迴往下找
-    if len(items) > 1:
-        updateFPtree(items[1::], fp_tree.child[items[0]], headPointTable, cnt)
-        
-#找到最後一個，再把targetNode放入
-def updateHeadPointTable(headPointBeginNode, targetNode):
-    while (headPointBeginNode.nextSimilarItem != None):    
-        headPointBeginNode = headPointBeginNode.nextSimilarItem
-    headPointBeginNode.nextSimilarItem = targetNode
-        
-#------------------挖掘頻繁項集------------------------------------------   
-   
-def mineFPTree(header:dict, prefix:set, frequent_set:set):
+    if len(data) > 1:
+        updateFPtree(data[1::], cur_root.child[data[0]], headPointTable, cnt)
+#找到最後一個，再把insertNode插入
+def updateHeadPointTable(originNode, insertNode):
+    while (originNode.nextSimilarItem != None):    
+        originNode = originNode.nextSimilarItem
+    originNode.nextSimilarItem = insertNode    
+    
+def mineFPTree(header:dict, prefix:set, frequent_set:set):#挖掘頻繁項集
     if len(prefix) >= PATTERN_LEN:
         return
     # val[0] for item name, val[1][0] for item count
@@ -117,10 +106,7 @@ def mineFPTree(header:dict, prefix:set, frequent_set:set):
             conditional_tree, conditional_header = createFPtree(prefix_path)
             if conditional_header is not None:
                 mineFPTree(conditional_header, new_prefix, frequent_set)
-
-#--------------------產生關連規則----------------------------------------
-
-def rule_generator(frequent_item, frequent_item_count, subset_counts):
+def rule_generator(frequent_item, frequent_item_count, subset_counts):#計算association rule數目的平行運算單元
     counter = 0
     subsets = [subset for i in range(1, len(frequent_item)) for subset in combinations(frequent_item, i)]
     for s in subsets:
@@ -132,7 +118,7 @@ def rule_generator(frequent_item, frequent_item_count, subset_counts):
                 counter += 1
     return counter
 
-def generateRulesParallel(frequent_set):
+def generateRulesParallel(frequent_set):#計算association rule總數目
     tasks = []
     for item in frequent_set:
         item_count = frequent_set[frozenset(item)]
@@ -146,18 +132,13 @@ def generateRulesParallel(frequent_set):
     total_counter = sum(results)
     print(total_counter)
 
-#---------------------計算每個長度的frequent_set-------------------------
-
-def cnt_each_len_freq_item(freq_set:set):
+def cnt_each_len_freq_item(freq_set:set):#計算每個長度的frequent_set
     cnt = [0,0,0,0,0,0,0,0,0,0,0]
     # cnt = [0,0,0,0,0,0]
     for item in freq_set:
         cnt[len(item)] += 1
     for idx in range(1,len(cnt)):
         print('|L^{}|={}'.format(idx,cnt[idx]))
-        
-#---------------------印出路徑-------------------------------------------
-
 #每個nextSimilarItem，都呼叫ascendTree往上找路徑
 def findPrefixPath(headPointTable, node): 
     condPaths = {}
@@ -174,11 +155,10 @@ def findPrefixPath(headPointTable, node):
         
     return condPaths 
 
-
 #把到root經過的點都加入prefixs
 def ascendTree(node): 
     prefixs = []
-    while (node.parent != None) and (node.parent.name != 'null set'):
+    while (node.parent != None) and (node.parent.name != -1):
         node = node.parent
         prefixs.append(node.name)
     return  prefixs   
@@ -191,7 +171,10 @@ if __name__=='__main__':
     frequent_set = {}
     prefix = set([])
     mineFPTree(headpointList, prefix, frequent_set)
+    cnt_each_len_freq_item(frequent_set)
+    print("frequent item set mining time: ",time.time() - begin_time)
+    frequent_item_minging_time=time.time()
     generateRulesParallel(frequent_set)
     
-    cnt_each_len_freq_item(frequent_set)
-    print(time.time() - begin_time)
+    print("association rule mining time: ",time.time() - frequent_item_minging_time)
+    print("Total time: ",time.time()-begin_time)
